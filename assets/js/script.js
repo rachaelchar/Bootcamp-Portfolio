8992e5fe2cee71192bd4f10b8cbf486fb387d185
RachaelChar <charewicz.r@gmail.com>
2020-01-26 15:38:15 -0500

    functions for changing card images on hover

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,14 @@
 $(document).ready(function () {
   $(".parallax").parallax();
+
+  var emsImg = $("#ems-img");
+
 });
 
+function hover(emsImg) {
+  emsImg.setAttribute("src", "assets/images/EMS_register.png");
+};
+
+function unhover(emsImg) {
+  emsImg.setAttribute("src", "assets/images/EMS_index.png");
+};
